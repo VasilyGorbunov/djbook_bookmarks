e51acc6eb3680c4b800ef2e03ae841406396aa97
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-10-30 13:26:37 +0300

    237

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -17,6 +17,7 @@ urlpatterns = [
     path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
     path('register/', views.register, name='register'),
+    path('edit/', views.edit, name='edit'),
 ]
 
 if settings.DEBUG:
