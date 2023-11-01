8de0d0a56b35f1dcafbcc691be66eab3d0f454be
VasilyGorbunov <vasily.gorbunov@gmail.com>
2023-11-01 12:54:48 +0300

    334

--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -1,6 +1,9 @@
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
+from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
 
 from images.forms import ImageCreateForm
 from images.models import Image
@@ -8,7 +11,7 @@ from images.models import Image
 
 @login_required
 def image_create(request):
-    if request.method== 'POST':
+    if request.method == 'POST':
         form = ImageCreateForm(data=request.POST)
         if form.is_valid():
             cd = form.cleaned_data
@@ -37,3 +40,49 @@ def image_detail(request, id, slug):
                       'section': 'images',
                       'image': image
                   })
+
+
+@login_required
+@require_POST
+def image_like(request):
+    image_id = request.POST.get('id')
+    action = request.POST.get('action')
+
+    if image_id and action:
+        try:
+            image = Image.objects.get(id=image_id)
+            if action == 'like':
+                image.users_like.add(request.user)
+            else:
+                image.users_like.remove(request.user)
+            return JsonResponse({'status': 'ok'})
+        except Image.DoesNotExist:
+            pass
+    return JsonResponse({'status': 'error'})
+
+
+@login_required
+def image_list(request):
+    images = Image.objects.all()
+    paginator = Paginator(images, 8)
+    page = request.GET.get('page')
+    images_only = request.GET.get('images_only')
+
+    try:
+        images = paginator.page(page)
+    except PageNotAnInteger:
+        images = paginator.page(1)
+    except EmptyPage:
+        if images_only:
+            return HttpResponse('')
+        images = paginator.page(paginator.num_pages)
+
+    if images_only:
+        return render(request,
+                      'images/image/list_images.html',
+                      {'section': 'images',
+                       'images': images})
+    return render(request,
+                  'images/image/list.html',
+                  {'section': 'images',
+                   'images': images})
